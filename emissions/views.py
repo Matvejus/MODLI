89d91f30f7b733477ec5203b67ba9f252396bd28
M.Mochalov <matvei@test.com>
2024-03-05 17:16:27 +0100

    calculator without usage

--- emissions/views.py
+++ emissions/views.py
@@ -24,6 +24,18 @@ def lowest_emissions(combinations):
 
     return lowest_emissions
 
+def optimal(combinations):
+    best_combo = float('inf')
+    best =[]
+
+    for c in combinations:
+        total = c['cost']+c['total_emissions']
+        if total < best_combo:
+            best.append(total)
+            best_combo = total
+    return best
+
+
 
 
 def calculator(request):
@@ -48,6 +60,12 @@ def calculator(request):
             elif lowest_emissions_boolean:
                 emissions = lowest_emissions(gowns)
                 context = {'gowns': emissions}
+                return render(request, 'calculator.html', context)
+            
+            elif lowest_cost_boolean and lowest_emissions_boolean:
+                best_combination = optimal(gowns)
+                context = {'gowns': best_combination}
+
                 return render(request, 'calculator.html', context)
 
     return render(request, 'calculator.html')
