643ca2f5b203e74fbce3deaa8477b56c19799f5e
M.Mochalov <matvei@test.com>
2024-07-03 20:41:29 +0200

    version

--- emissions/templatetags/custom_filters.py
+++ emissions/templatetags/custom_filters.py
@@ -9,3 +9,10 @@ def get_item(dictionary, key):
 @register.filter
 def has_certification(gown, certification_name):
     return gown.certificates.filter(name=certification_name).exists()
+
+@register.filter
+def get_emission_value(emissions_dict, args):
+    gown_id, attribute = args.split('.')
+    gown_id = int(gown_id)
+    emission = emissions_dict.get(gown_id)
+    return getattr(emission, attribute, None)
