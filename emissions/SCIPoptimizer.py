527a470c2c5dada9d8a86e6009d23ac5f433c7f8
Roel Lambers <r.lambers2@hva.nl>
2024-10-18 11:33:59 +0200

    Added features to optimizer

--- emissions/SCIPoptimizer.py
+++ emissions/SCIPoptimizer.py
@@ -36,10 +36,11 @@ class Gown:
         self.Impacts = impacts
 
 class Specs:
-    def __init__(self, usage_per_week, pickups_per_week,optimizer=[Envpar.MONEY]):
+    def __init__(self, usage_per_week, pickups_per_week,optimizer=[Envpar.MONEY],loss_percentage = 0.001):
         self.usage_per_week = usage_per_week
         self.pickups_per_week = pickups_per_week
         self.usage_between_pickup = self.usage_per_week / self.pickups_per_week
+        self.loss_percentage = loss_percentage
         self.optimizer = optimizer
 
 #Creates cartesian product of all lists entered if multiple are given in *lsts. 
@@ -90,6 +91,7 @@ Time = range(timesteps)
 
 #Create Model
 md = ps.Model("gown_optimization")
+md.hideOutput(True)
 
 #Create environmental cost vars
 def make_cost_vars(Options):
@@ -163,7 +165,6 @@ def balance_reusable(dv,mn,mx, md = md):
         mn = 0.1
         mx = 0.9
 
-
     md.addCons(quicksum(dv[Stages.USAGE][x, t] for t in Time for x in Options if x.Reusable) >= 
                mn * quicksum(dv[Stages.USAGE][x,t] for t in Time for x in Options), name=f"BALANCE_reusable")
     md.addCons(quicksum(dv[Stages.USAGE][x, t] for t in Time for x in Options if x.Reusable) <= 
@@ -171,35 +172,61 @@ def balance_reusable(dv,mn,mx, md = md):
         
     return md
 
+def estimate_losses(lost,md = md):
+    if 1==0:
+        for x in Options:
+            app_time = [t for t in Time if t > x.Longevity]
+            for t in app_time:
+                md.addCons(dv[Stages.EOL][x,t] >= x.Longevity*dv[Stages.USAGE][x,t-1])
+                md.addCons(dv[Stages.EOL][x,t] <= 1 + 2*x.Longevity*dv[Stages.USAGE][x,t-1])
+    return md
+
+def build_buffer(dv,factor=10,md = md):
+    for t in Time:
+        if t > 1:
+            md.addCons(quicksum([dv[Stages.HOSPITAL][x,t] for x in Options]) >= factor*Specifications.usage_between_pickup)
+    return md
+
+def estimate_gown_loss(dv,md=md):
+    for x in Options:
+        for t in Time:
+            if t%10 == 9:
+                md.addCons(quicksum([dv[Stages.LOST][x,t-tx] - 
+                                     Specifications.loss_percentage*dv[Stages.USAGE][x,t-tx-1] for tx in range(0,9)]) >=0)
+    return md
+            
 #Make decision and cost-variables
 dv = make_decision_vars(Options)
 cv = make_cost_vars(Options)
 
 #Set the constraints.
-#First, fix the number of gowns at the hospital at time 0; default is 50 gowns per type.
+#First, fix the number of gowns at the hospital at time 0.
 #Add the constraints that manage the gown flow: LAUNDRY, USAGE, LOST, EOL, etcetera.
 #Calculate the cost and add them as constraints for the cost-variables. 
 #Give a window of what percentage of the gowns used should be reusable; default between 10-90%.
 md = initial_settings(Options,dv,[2000,0,0])
 md = gown_flow(dv)
 md = calculate_costs(dv,cv)
-md = balance_reusable(dv,0.2,0.7)
+md = balance_reusable(dv,0.6,1)
+md = build_buffer(dv)
+md = estimate_gown_loss(dv)
 
 # Set objective to minimize all environmental impacts and costs
 md.setObjective(quicksum(cv["TOTAL"][x][cs] for cs in Specifications.optimizer for x in Options),'minimize')
+md.setParam('limits/time', 120)
 
 # Optimize the model
 md.optimize()
 
 # Check if the model is optimized successfully
-if md.getStatus() != "optimal":
+if md.getStatus() == "infeasible":
     print("Model is infeasible. Analyzing infeasibilities...")
     md.computeIIS()
     md.write("model.ilp")
     for c in md.getConstrs():
         if c.IISConstr:
             print(f"Infeasible constraint: {c.constrName}")
-elif md.getStatus() == "optimal":
+else:
     print("Results of gown usage for each variant:")
     for x in Options:
         usage_values = [int(md.getVal(dv[Stages.USAGE][x, t])) for t in Time]
@@ -211,8 +238,6 @@ elif md.getStatus() == "optimal":
         print("Total impacts:")
         for cs in Envpar:
             print(f"{cs.name}: {md.getVal(total_impact[x][cs])}")
-else:
-    print("Optimization was not successful. Status code:", md.getStatus())
 
 # Identify the best option based on the total costs and impacts
 #best_option = min(Options, key=lambda x: sum(md.getVal(cv["TOTAL"][x][cs]) for cs in Envpar))
